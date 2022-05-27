6dff0883358712abb58dff737bd462f589ec108c
JSamuelCaV <92724791+JSamuelCaV@users.noreply.github.com>
2022-05-27 22:40:28 +0200

    Sprint #4 terminado
    implementados los editText no editables con el resto de datos
    subido el post de los datos al mockapi

---
+++ app/src/main/java/com/example/yugimarket/NavigationDrawerActivity.java
@@ -0,0 +1,114 @@
+package com.example.yugimarket;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class NavigationDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, DrawerLayout.DrawerListener {
+    private DrawerLayout drawerLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navView = findViewById(R.id.navigation_view);
+        navView.setNavigationItemSelectedListener(this);
+
+       MenuItem menuItem = navView.getMenu().getItem(0);
+        onNavigationItemSelected(menuItem);
+
+        menuItem.setChecked(true);
+
+        drawerLayout.addDrawerListener(this);
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+
+        }
+    }
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment=null;
+                int title;
+                switch (item.getItemId()) {
+                        case R.id.find_cards:
+                            fragment=new FragmentFindCards();
+                            title = R.string.find_cards;
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            getSupportActionBar().setTitle(title);
+                            callFragmnent(fragment);
+
+                        break;
+                        case R.id.upload_cards:
+                            title = R.string.upload_cards;
+                            fragment=new FragmentUploadCards();
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                            getSupportActionBar().setTitle(title);
+                            callFragmnent(fragment);
+                        break;
+
+                        default:
+                            throw new IllegalArgumentException("menu option not implement");
+                }
+                return true;
+            }
+
+
+            public void callFragmnent(Fragment fragment){
+
+                FragmentManager manager =getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.linear_layout, fragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+    @Override
+    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View drawerView) {
+        Toast.makeText(this, getString(R.string.navigation_drawer_open),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View drawerView) {
+        Toast.makeText(this, getString(R.string.navigation_drawer_close),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
+
+    }
+}
---
+++ app/src/main/java/com/example/yugimarket/SpecificUploadActivity.java
@@ -0,0 +1,152 @@
+package com.example.yugimarket;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class SpecificUploadActivity extends AppCompatActivity {
+    private EditText editTextName;
+    private EditText editTextCode;
+
+    String image_url_small,name,set_code;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specific_upload);
+        editTextName = (EditText) findViewById(R.id.name);
+        editTextCode = (EditText) findViewById(R.id.setCode);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            name = (String) bundle.get("name");
+            set_code = (String) bundle.get("set_code");
+
+            editTextName.setText(name);
+
+            editTextCode.setText(set_code);
+
+        }
+
+
+        final Button button=findViewById(R.id.upload_button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String sseller,sprice,snumber_to_sell;
+                Double dprice ;
+                Integer inumber_to_sell;
+
+                EditText price=findViewById(R.id.price);
+                sprice=price.getText().toString();
+                try {
+                    dprice = Double.parseDouble(sprice);
+
+                } catch (NumberFormatException nfenpe) {
+                    Toast.makeText(view.getContext(), "You did not enter a number in price", Toast.LENGTH_SHORT).show();
+                    dprice = 0.0;
+
+
+
+                }
+                EditText number_to_sell=findViewById(R.id.number_to_sell);
+                snumber_to_sell=number_to_sell.getText().toString();
+                try {
+                    inumber_to_sell = Integer.parseInt(snumber_to_sell);
+                }catch (NumberFormatException nfe){
+                    Toast.makeText(view.getContext(),"You did not enter a number in number_to_sell",Toast.LENGTH_SHORT).show();
+                    inumber_to_sell=0;
+                }
+
+                EditText seller=findViewById(R.id.seller);
+                sseller=seller.getText().toString();
+                if(sseller.matches("")){
+                    Toast.makeText(view.getContext(),"You did not enter a seller",Toast.LENGTH_SHORT).show();
+
+                }
+                if (dprice!=0.0 && inumber_to_sell!=0 && sseller!="") {
+
+
+                    UploadCards(view ,name, set_code ,dprice,inumber_to_sell,sseller,image_url_small);
+
+
+
+                }
+
+
+            }
+        });
+    }
+    private void UploadCards(View view,String name,String set_code,Double fprice,int inumber_to_sell,String sseller,String image_url_small){
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("Card_id", set_code);
+            jsonObject.put("Card_name", name);
+            jsonObject.put("price", fprice);
+            jsonObject.put("number_to_sell", inumber_to_sell);
+            jsonObject.put("seller", sseller);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String url="https://624ad3a4852fe6ebf88bbd03.mockapi.io/offers";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,jsonObject,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(view.getContext(), "uploading data",Toast.LENGTH_SHORT).show();
+                        Intent intent= new Intent(view.getContext(),FragmentUploadCards.class);
+                        startActivity(intent);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(view.getContext(),"could failed the uploading data"+error.toString(),Toast.LENGTH_SHORT).show();
+                        Log.d("Error: ",error.toString());
+
+                    }
+
+                });
+
+
+
+
+        queue.add(jsonObjectRequest);
+    }
+
+
+    public void onClick(View view) {
+    }
+
+
+}
+
